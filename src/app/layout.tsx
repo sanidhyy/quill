500b98f55c13a238efe7f1c1b54dc18762dce416
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-04-04 13:04:13 +0000

    max width wrapper component implemented

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -15,8 +18,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="light">
+      <body
+        className={cn(
+          "min-h-screen font-sans antialiased grainy",
+          inter.className
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/components/max-width-wrapper.tsx
@@ -0,0 +1,20 @@
+import { cn } from "@/lib/utils";
+import type { PropsWithChildren } from "react";
+
+export const MaxWidthWrapper = ({
+  className,
+  children,
+}: PropsWithChildren<{
+  className?: string;
+}>) => {
+  return (
+    <div
+      className={cn(
+        "mx-auto w-full max-w-screen-xl px-2.5 md:px-20",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
