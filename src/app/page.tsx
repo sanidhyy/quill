f34fc5cb7d8d97f9a483bf0564312a9aac026f81
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-04-04 14:10:18 +0000

    navbar ui implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -147,6 +147,7 @@ export default function Home() {
           </li>
         </ol>
 
+        {/* upload preview showcase */}
         <div className="mx-auto max-w-6xl px-6 lg:px-8">
           <div className="mt-16 flow-root sm:mt-24">
             <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
