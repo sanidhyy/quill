69c5effc4f9de97920de656700ac8c4db5eadc63
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-04-10 07:03:58 +0000

    worker added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+
+    return config;
+  },
+};
 
 export default nextConfig;
