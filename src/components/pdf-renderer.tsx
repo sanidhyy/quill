c9498e78aa44c523204018695c10928d131576d7
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-04-10 03:45:44 +0000

    pdf renderer topbar added

--- src/components/pdf-renderer.tsx
+++ src/components/pdf-renderer.tsx
@@ -1,3 +1,10 @@
 export const PDFRenderer = () => {
-  return <div className="">PDFRenderer</div>;
+  return (
+    <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
+      {/* topbar */}
+      <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
+        <div className="flex items-center gap-1.5">topbar</div>
+      </div>
+    </div>
+  );
 };
