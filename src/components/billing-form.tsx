994ea54d575d30a02f2c861c95d7ee9f923afbe1
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-04-12 16:38:49 +0000

    billing page created

---
+++ src/components/billing-form.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { format } from "date-fns";
+import { Loader2 } from "lucide-react";
+import { toast } from "sonner";
+
+import { trpc } from "@/app/_trpc/client";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import type { getUserSubscriptionPlan } from "@/lib/stripe";
+
+import { MaxWidthWrapper } from "./max-width-wrapper";
+
+type BillingFormProps = {
+  subscriptionPlan: Awaited<ReturnType<typeof getUserSubscriptionPlan>>;
+};
+
+export const BillingForm = ({ subscriptionPlan }: BillingFormProps) => {
+  const { mutate: createStripeSession, isPending } =
+    trpc.createStripeSession.useMutation({
+      onSuccess: ({ url }) => {
+        if (url) window.location.href = url;
+        else
+          toast.error("There was a problem...", {
+            description: "Please try again in a moment.",
+          });
+      },
+    });
+  return (
+    <MaxWidthWrapper className="max-w-5xl">
+      <form
+        className="mt-12"
+        onSubmit={(e) => {
+          e.preventDefault();
+
+          createStripeSession();
+        }}
+      >
+        <Card>
+          <CardHeader>
+            <CardTitle>Subscription Plan</CardTitle>
+            <CardDescription>
+              You&apos;re currently on the{" "}
+              <strong>{subscriptionPlan.name}</strong> plan.
+            </CardDescription>
+          </CardHeader>
+
+          <CardFooter className="flex flex-col items-start space-y-2 md:flex-row md:justify-between md:space-x-0">
+            <Button
+              disabled={isPending}
+              aria-disabled={isPending}
+              type="submit"
+            >
+              {isPending ? (
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              ) : null}
+
+              {subscriptionPlan.isSubscribed
+                ? "Manage Subscription"
+                : "Upgrade to Pro"}
+            </Button>
+
+            {subscriptionPlan.isSubscribed ? (
+              <p className="rounded-full text-xs font-medium">
+                {subscriptionPlan.isCanceled
+                  ? "Your plan will be canceled on "
+                  : "Your plean renews on "}
+                {format(subscriptionPlan.stripeCurrentPeriodEnd!, "dd.MM.yyyy")}
+                .
+              </p>
+            ) : null}
+          </CardFooter>
+        </Card>
+      </form>
+    </MaxWidthWrapper>
+  );
+};
