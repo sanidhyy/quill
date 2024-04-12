ad6defd635d6e6f5c85540e4e965189b02fb6c88
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-04-12 12:33:09 +0000

    exported imp types

--- src/types/message.ts
+++ src/types/message.ts
@@ -3,7 +3,7 @@ import { inferRouterOutputs } from "@trpc/server";
 
 type RouterOutput = inferRouterOutputs<AppRouter>;
 
-type Messages = RouterOutput["getFileMessages"]["messages"];
+export type Messages = RouterOutput["getFileMessages"]["messages"];
 
 type OmitText = Omit<Messages[number], "text">;
 
