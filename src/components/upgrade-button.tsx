5c1c936c8a6e48c498b4b11632d2566d688b3820
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-04-12 16:17:58 +0000

    stripe upgrade button functionality implemented

--- src/components/upgrade-button.tsx
+++ src/components/upgrade-button.tsx
@@ -1,10 +1,44 @@
-import { ArrowRight } from "lucide-react";
+"use client";
 
+import { ArrowRight } from "lucide-react";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+
+import { trpc } from "@/app/_trpc/client";
 import { Button } from "@/components/ui/button";
 
+const CHECKOUT_TOAST_ID = "checkout-toast";
+
 export const UpgradeButton = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const { mutate: createStripeSession } = trpc.createStripeSession.useMutation({
+    onSuccess: ({ url }) => {
+      window.location.href = url ?? "/dashboard/billing";
+    },
+    onSettled: () => setIsLoading(false),
+  });
+
+  const handleClick = () => {
+    setIsLoading(true);
+    toast.loading("Redirecting to checkout...", {
+      description: "Please wait while we redirect you to checkout page.",
+      id: CHECKOUT_TOAST_ID,
+    });
+
+    createStripeSession();
+  };
+
+  useEffect(() => {
+    if (!isLoading) toast.dismiss(CHECKOUT_TOAST_ID);
+  }, [isLoading]);
+
   return (
-    <Button className="w-full">
+    <Button
+      disabled={isLoading}
+      aria-disabled={isLoading}
+      className="w-full"
+      onClick={handleClick}
+    >
       Upgrade now <ArrowRight className="h-5 w-5 ml-1.5" />
     </Button>
   );
