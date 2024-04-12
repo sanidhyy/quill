fb13d6de3d2555c5b26c09369b3b0c7cfc9db9b3
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-04-12 12:47:56 +0000

    chat messages infinite queries setup

--- src/components/chat/messages.tsx
+++ src/components/chat/messages.tsx
@@ -1,6 +1,7 @@
+import { useIntersection } from "@mantine/hooks";
 import { keepPreviousData } from "@tanstack/react-query";
 import { Loader2, MessageSquare } from "lucide-react";
-import { useContext } from "react";
+import { useContext, useEffect, useRef } from "react";
 import Skeleton from "react-loading-skeleton";
 
 import { trpc } from "@/app/_trpc/client";
@@ -42,6 +43,19 @@ export const Messages = ({ fileId }: { fileId: string }) => {
     ...(messages ?? []),
   ];
 
+  const lastMessageRef = useRef<HTMLDivElement>(null);
+
+  const { ref, entry } = useIntersection({
+    root: lastMessageRef.current,
+    threshold: 1,
+  });
+
+  useEffect(() => {
+    if (entry?.isIntersecting) {
+      fetchNextPage();
+    }
+  }, [entry, fetchNextPage]);
+
   return (
     <div className="flex max-h-[calc(100vh-3.5rem-7rem)] border-zinc-200 flex-1 flex-col-reverse gap-4 p-4 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch">
       {combinedMessages && combinedMessages.length > 0 ? (
@@ -53,6 +67,7 @@ export const Messages = ({ fileId }: { fileId: string }) => {
           if (i === combinedMessages.length - 1) {
             return (
               <Message
+                ref={ref}
                 key={message.id}
                 isNextMessageSamePerson={isNextMessageSamePerson}
                 message={message}
