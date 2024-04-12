e7dec70d0e6d35a1e76dd2c2dad4c5c70f7a2281
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-04-12 10:36:56 +0000

    chat input fetch request made

--- src/components/chat/chat-input.tsx
+++ src/components/chat/chat-input.tsx
@@ -1,13 +1,22 @@
-import { Send } from "lucide-react";
+"use client";
 
-import { Textarea } from "@/components/ui/textarea";
+import { Send } from "lucide-react";
+import { useContext, useRef } from "react";
+
 import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+
+import { ChatContext } from "./chat-context";
 
 type ChatInputProps = {
   isDisabled?: boolean;
 };
 
 export const ChatInput = ({ isDisabled }: ChatInputProps) => {
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const { addMessage, handleInputChange, isLoading, message } =
+    useContext(ChatContext);
+
   return (
     <div className="absolute bottom-0 left-0 w-full">
       <form className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
@@ -15,16 +24,39 @@ export const ChatInput = ({ isDisabled }: ChatInputProps) => {
           <div className="relative flex flex-col w-full flex-grow p-4">
             <div className="relative">
               <Textarea
+                ref={textareaRef}
                 placeholder="Enter your question..."
                 rows={1}
                 maxRows={4}
                 autoFocus
+                disabled={isLoading || isDisabled}
+                aria-disabled={isLoading || isDisabled}
+                onChange={handleInputChange}
+                value={message}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter" && !e.shiftKey) {
+                    e.preventDefault();
+
+                    addMessage();
+
+                    textareaRef.current?.focus();
+                  }
+                }}
                 className="resize-none pr-12 text-base py-3 scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch"
               />
 
               <Button
+                disabled={isLoading || isDisabled}
+                aria-disabled={isLoading || isDisabled}
+                type="submit"
+                onClick={() => {
+                  addMessage();
+
+                  textareaRef.current?.focus();
+                }}
                 className="absolute bottom-1.5 right-[8px]"
                 aria-label="Send Message..."
+                title="Send Message..."
               >
                 <Send className="h-4 w-4" />
               </Button>
