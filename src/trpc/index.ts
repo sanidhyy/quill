21666bd48b96070a945eb5474222b8907991577a
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-04-12 11:37:54 +0000

    get file messages api endpoint created

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -3,6 +3,7 @@ import { TRPCError } from "@trpc/server";
 import { UTApi } from "uploadthing/server";
 import * as z from "zod";
 
+import { INFINITE_QUERY_LIMIT } from "@/app/config/infinite-query";
 import { db } from "@/db";
 
 import { privateProcedure, procedure, router } from "./trpc";
@@ -103,6 +104,58 @@ export const appRouter = router({
 
       return {
         status: file.uploadStatus,
+      };
+    }),
+  getFileMessages: privateProcedure
+    .input(
+      z.object({
+        limit: z.number().min(1).max(100).nullish(),
+        cursor: z.string().nullish(),
+        fileId: z.string(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const { userId } = ctx;
+      const { fileId, cursor } = input;
+      const limit = input.limit ?? INFINITE_QUERY_LIMIT;
+
+      const file = await db.file.findUnique({
+        where: {
+          id: fileId,
+          userId,
+        },
+      });
+
+      if (!file) throw new TRPCError({ code: "NOT_FOUND" });
+
+      const messages = await db.message.findMany({
+        take: limit + 1,
+        where: {
+          fileId,
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+        cursor: cursor ? { id: cursor } : undefined,
+        select: {
+          id: true,
+          isUserMessage: true,
+          createdAt: true,
+          text: true,
+        },
+      });
+
+      let nextCursor: typeof cursor | undefined = undefined;
+
+      if (messages.length > limit) {
+        const nextItem = messages.pop();
+
+        nextCursor = nextItem?.id;
+      }
+
+      return {
+        messages,
+        nextCursor,
       };
     }),
 });
