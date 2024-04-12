9e10e4e12e9a8f19f4061fc808602a0126c406a9
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-04-12 07:08:16 +0000

    chat input ui completed

--- src/components/chat/chat-input.tsx
+++ src/components/chat/chat-input.tsx
@@ -1,7 +1,37 @@
+import { Send } from "lucide-react";
+
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+
 type ChatInputProps = {
   isDisabled?: boolean;
 };
 
 export const ChatInput = ({ isDisabled }: ChatInputProps) => {
-  return <div className="">ChatInput</div>;
+  return (
+    <div className="absolute bottom-0 left-0 w-full">
+      <form className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
+        <div className="relative flex h-full flex-1 items-stretch flex-col">
+          <div className="relative flex flex-col w-full flex-grow p-4">
+            <div className="relative">
+              <Textarea
+                placeholder="Enter your question..."
+                rows={1}
+                maxRows={4}
+                autoFocus
+                className="resize-none pr-12 text-base py-3 scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch"
+              />
+
+              <Button
+                className="absolute bottom-1.5 right-[8px]"
+                aria-label="Send Message..."
+              >
+                <Send className="h-4 w-4" />
+              </Button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,24 +1,27 @@
-import * as React from "react"
+import * as React from "react";
+import TextareaAutosize, {
+  type TextareaAutosizeProps,
+} from "react-textarea-autosize";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 export interface TextareaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
-const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaAutosizeProps>(
   ({ className, ...props }, ref) => {
     return (
-      <textarea
+      <TextareaAutosize
         className={cn(
-          "flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          "flex w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
         ref={ref}
         {...props}
       />
-    )
+    );
   }
-)
-Textarea.displayName = "Textarea"
+);
+Textarea.displayName = "Textarea";
 
-export { Textarea }
+export { Textarea };
