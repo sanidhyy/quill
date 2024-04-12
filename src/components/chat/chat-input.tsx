6032325e578763c9da256a90336d39b0a0fbe479
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-04-12 11:38:35 +0000

    chat messages ui completed and fetched messages from api

--- src/components/chat/chat-input.tsx
+++ src/components/chat/chat-input.tsx
@@ -19,7 +19,12 @@ export const ChatInput = ({ isDisabled }: ChatInputProps) => {
 
   return (
     <div className="absolute bottom-0 left-0 w-full">
-      <form className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        autoCapitalize="off"
+        autoComplete="off"
+        className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl"
+      >
         <div className="relative flex h-full flex-1 items-stretch flex-col">
           <div className="relative flex flex-col w-full flex-grow p-4">
             <div className="relative">
