1ef9b2ee09e71bb864ace530b14eebe926e899bb
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-04-12 14:10:15 +0000

    stripe config added

---
+++ src/config/stripe.ts
@@ -0,0 +1,28 @@
+export const PLANS = [
+  {
+    name: "Free",
+    slug: "free",
+    quota: 10,
+    pagesPerPdf: 5,
+    price: {
+      amount: 0.0,
+      priceIds: {
+        test: "",
+        production: "",
+      },
+    },
+  },
+  {
+    name: "Pro",
+    slug: "pro",
+    quota: 50,
+    pagesPerPdf: 25,
+    price: {
+      amount: 14.0,
+      priceIds: {
+        test: process.env.STRIPE_PRICE_ID!,
+        production: "",
+      },
+    },
+  },
+];
