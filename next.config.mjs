27397851e74e0d30d60c3fef69778374ad8a5460
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-04-13 03:51:54 +0000

    added google image avatar to remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,14 @@ const nextConfig = {
 
     return config;
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
