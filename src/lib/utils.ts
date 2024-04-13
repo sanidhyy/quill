192dac2ea1a9160f2e11cbf8de6f0859a8926752
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-04-13 04:40:59 +0000

    site metadata updated

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx";
+import { type Metadata } from "next";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -7,4 +8,44 @@ export function cn(...inputs: ClassValue[]) {
 
 export function absoluteUrl(path: string) {
   return `${process.env.NEXT_PUBLIC_BASE_URL}${path}`;
+}
+
+export function constructMetadata({
+  title = "Quill - the SaaS for students",
+  description = "Quill is an open-source software to make chatting to your PDF files easy.",
+  image = "/thumbnail.png",
+  noIndex = false,
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  noIndex?: boolean;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@TechnicalShubam",
+    },
+    metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL),
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  };
 }
