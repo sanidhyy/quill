e812b34467430b48ac62677393b602cf4fd976d5
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2026-06-30 17:58:36 +0530

    fix: build

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -5,7 +5,7 @@ import { PLANS } from "@/config/stripe";
 import { db } from "@/db";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "", {
-  apiVersion: "2026-04-22.dahlia",
+  apiVersion: "2026-06-24.dahlia",
   typescript: true,
 });
 
