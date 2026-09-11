804067f1b2a8b94c9cbb43d7017bf12d6a9aa7c8
Sanidhya Kumar Verma <sanidhyyy@gmail.com>
2026-09-11 22:53:49 +0530

    chore: rename next.config.mjs to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   async redirects() {
     return [
       {
@@ -14,6 +15,7 @@ const nextConfig = {
       },
     ];
   },
+  devIndicators: false,
   turbopack: {
     resolveAlias: {
       canvas: "false",
